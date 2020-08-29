63fb494289135907a9a9298a8beb79d7a707e4bf
Foxy <thefoxy41@gmail.com>
2020-08-30 00:56:18 +0200

    Add bootstrap

--- server/resources/views/layout/main.blade.php
+++ server/resources/views/layout/main.blade.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>La Ferme des 3 Châtaigniers</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link href="{{ asset('font/nunito.css') }}" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('favicon.png') }}" rel="icon">
 </head>
 <body>
 
