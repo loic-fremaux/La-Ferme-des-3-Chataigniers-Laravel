4811ee97336a9e19842d1f7a547e476a5efb68fa
isox <theo.meunier41@gmail.com>
2020-08-30 20:36:14 +0200

    ajout du scss dans tout les fichier mettre a jours le main.blade.php pour que le scss fonctionne

--- server/resources/views/layout/main.blade.php
+++ server/resources/views/layout/main.blade.php
@@ -28,6 +28,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <link href="{{ asset('favicon.png') }}" rel="icon">
 </head>
@@ -40,9 +41,9 @@
             <li><a href="{{ url('/elevage') }}">L'élevage de porcs</a></li>
             <li><a href="{{ url('/vente_a_la_boutique') }}">Vente à la boutique ▼</a>
                 <ul class="unroll">
-                    <li><a href="/vente_a_la_boutique/pates_et_plat_cuisines">Pâtés et plats cuisinés</a></li>
-                    <li><a href="/vente_a_la_boutique/viande_au_detail">Viande au détail</a></li>
-                    <li><a href="/vente_a_la_boutique/caisettes_de_viande">Caissettes de viande</a></li>
+                    <li><a href="{{ url('/pates_et_plats_cuisines') }}">Pâtés et plats cuisinés</a></li>
+                    <li><a href="{{ url('/viande_detaille') }}">Viande au détail</a></li>
+                    <li><a href="{{ url('/caissette_de_viande') }}">Caissettes de viande</a></li>
                 </ul>
             </li>
         </ul>
