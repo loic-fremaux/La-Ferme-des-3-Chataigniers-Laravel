744aacedc00c0dded4b1c13950dc7134afde4278
isox <theo.meunier41@gmail.com>
2020-09-01 00:38:40 +0200

    clear code

--- server/resources/views/elevage.blade.php
+++ server/resources/views/elevage.blade.php
@@ -1,120 +1,114 @@
 @extends('layout.main')
 
 @section('content')
-<div id="wrapper">
+    <div class="title_group">
+        <div class="title_line"></div>
+        <h2>L'élevage</h2>
+        <div class="right_line"></div>
+    </div>
 
-    <section class="content_wrapper">
-        <div class="title_group">
-            <div class="title_line"></div>
-            <h2>L'élevage</h2>
-            <div class="right_line"></div>
-        </div>
-
-        <div class="contact_infos">
-            <div>
-                <p>
-                    <strong>Emmanuel</strong> élève ses porcs sur paille comme autrefois : dans ces conditions, les
-                    animaux bénéficient d'un confort qui leur permet d'exprimer leurs comportements naturels.
-                    <br> Élevés sans
-                    antibiotiques, ils sont nourris avec les céréales de la ferme et de la graine de lin.<br>
-                    Riche en oméga 3, le lin donne à la viande une saveur et un moelleux incomparable.
-                </p>
-            </div>
-        </div>
+    <div class="contact_infos">
         <div>
-
-            <?php
-
-            $list = [
-                [
-                    "1", "slicer1.jpg"
-                ], [
-                    "2", "slicer2.jpg"
-                ], [
-                    "3", "slicer3.jpg"
-                ], [
-                    "4", "slicer4.jpg"
-                ], [
-                    "5", "slicer5.jpg"
-                ], [
-                    "6", "slicer6.jpg"
-                ], [
-                    "7", "slicer7.jpg"
-                ], [
-                    "8", "slicer8.jpg"
-                ]
-            ]
-            ?>
-
-            <div class="slideshow-container">
-                <?php foreach ($list as $key): ?>
-                <div class="mySlides fade">
-                    <img class="img_slider" src="/img/elevage/<?= $key[1] ?>">
-                    <div class="text_slicer"><?= $key[0] ?> / 8</div>
-                </div>
-                <?php endforeach; ?>
-
-            </div>
-            <br>
-
-            <div style="text-align:center">
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-                <span class="dot"></span>
-            </div>
-
-            <script>
-                var slideIndex = 0;
-                showSlides();
-
-                function showSlides() {
-                    var i;
-                    var slides = document.getElementsByClassName("mySlides");
-                    var dots = document.getElementsByClassName("dot");
-                    for (i = 0; i < slides.length; i++) {
-                        slides[i].style.display = "none";
-                    }
-                    slideIndex++;
-                    if (slideIndex > slides.length) {
-                        slideIndex = 1
-                    }
-                    for (i = 0; i < dots.length; i++) {
-                        dots[i].className = dots[i].className.replace(" active", "");
-                    }
-                    slides[slideIndex - 1].style.display = "block";
-                    dots[slideIndex - 1].className += " active";
-                    setTimeout(showSlides, 3000); // Change image every 2 seconds
-                }
-            </script>
-        </div>
-
-
-        <div class="contact_infos">
-
-            <p><strong>Jean-Marie</strong> s'occupe des cultures : céréales et oléo protéagineux, sur près de 180
-                hectares.<br>
-                Il pratique une agriculture raisonnée qui se traduit par un apport réduit et contrôlé des intrants
-                et
-                pesticides.<br><br>
-                La majeure partie des cultures sert à composer les rations de nourriture des porcs.
+            <p>
+                <strong>Emmanuel</strong> élève ses porcs sur paille comme autrefois : dans ces conditions, les
+                animaux bénéficient d'un confort qui leur permet d'exprimer leurs comportements naturels.
+                <br> Élevés sans
+                antibiotiques, ils sont nourris avec les céréales de la ferme et de la graine de lin.<br>
+                Riche en oméga 3, le lin donne à la viande une saveur et un moelleux incomparable.
             </p>
-
-            <div class="flex_bottom">
-                <img class="img_bottom" src="/img/elevage/image1.jpg" alt="cerial">
-                <img class="img_bottom" src="/img/elevage/image2.jpg" alt="cerial">
-                <img class="img_bottom" src="/img/elevage/image3.jpg" alt="cerial">
-                <img class="img_bottom" src="/img/elevage/image4.jpg" alt="cerial">
-                <img class="img_bottom" src="/img/elevage/image5.jpg" alt="cerial">
-                <img class="img_bottom" src="/img/elevage/image6.jpg" alt="cerial">
-                <img class="img_bottom" src="/img/elevage/image7.jpg" alt="cerial">
-            </div>
         </div>
-    </section>
+    </div>
+    <div>
 
-</div>
+        <?php
+
+        $list = [
+            [
+                "1", "slicer1.jpg"
+            ], [
+                "2", "slicer2.jpg"
+            ], [
+                "3", "slicer3.jpg"
+            ], [
+                "4", "slicer4.jpg"
+            ], [
+                "5", "slicer5.jpg"
+            ], [
+                "6", "slicer6.jpg"
+            ], [
+                "7", "slicer7.jpg"
+            ], [
+                "8", "slicer8.jpg"
+            ]
+        ]
+        ?>
+
+        <div class="slideshow-container">
+            <?php foreach ($list as $key): ?>
+            <div class="mySlides fade">
+                <img class="img_slider" src="/img/elevage/<?= $key[1] ?>">
+                <div class="text_slicer"><?= $key[0] ?> / 8</div>
+            </div>
+            <?php endforeach; ?>
+
+        </div>
+        <br>
+
+        <div style="text-align:center">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+
+        <script>
+            var slideIndex = 0;
+            showSlides();
+
+            function showSlides() {
+                var i;
+                var slides = document.getElementsByClassName("mySlides");
+                var dots = document.getElementsByClassName("dot");
+                for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+                slideIndex++;
+                if (slideIndex > slides.length) {
+                    slideIndex = 1
+                }
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+                slides[slideIndex - 1].style.display = "block";
+                dots[slideIndex - 1].className += " active";
+                setTimeout(showSlides, 3000); // Change image every 2 seconds
+            }
+        </script>
+    </div>
+
+
+    <div class="contact_infos">
+
+        <p><strong>Jean-Marie</strong> s'occupe des cultures : céréales et oléo protéagineux, sur près de 180
+            hectares.<br>
+            Il pratique une agriculture raisonnée qui se traduit par un apport réduit et contrôlé des intrants
+            et
+            pesticides.<br><br>
+            La majeure partie des cultures sert à composer les rations de nourriture des porcs.
+        </p>
+
+        <div class="flex_bottom">
+            <img class="img_bottom" src="/img/elevage/image1.jpg" alt="cerial">
+            <img class="img_bottom" src="/img/elevage/image2.jpg" alt="cerial">
+            <img class="img_bottom" src="/img/elevage/image3.jpg" alt="cerial">
+            <img class="img_bottom" src="/img/elevage/image4.jpg" alt="cerial">
+            <img class="img_bottom" src="/img/elevage/image5.jpg" alt="cerial">
+            <img class="img_bottom" src="/img/elevage/image6.jpg" alt="cerial">
+        </div>
+    </div>
+
 @endsection
--- server/resources/views/vente_a_la_boutique/viande_detaille.blade.php
+++ server/resources/views/vente_a_la_boutique/viande_detaille.blade.php
@@ -9,91 +9,207 @@
     <?php
     $list = [
         [
-            'Boudin noir', ' conditionné sous vide par 2 poids 360g environ', 'boudin_noir.jpg', 'boudin blanc'
+            'Boudin noir',
+            'Sous vide par lot de 2 ',
+            '360g environ',
+            'boudin_noir.jpg',
+            'boudin blanc'
         ],
         [
-            'Chair à saucisse', ' conditionnée en sachet sous vide de 500g environ', 'chaire_saucisse.jpg', 'chaire a saucisse'
+            'Chair à saucisse',
+            'Sous vide',
+            '500g environ',
+            'chaire_saucisse.jpg',
+            'chaire a saucisse'
         ],
         [
-            'Chipolatas', ' conditionnées sous vide par 6 poids 360g environ', 'chipolatas.jpg', 'chipolatas'
+            'Chipolatas',
+            'Sous vide par lot de 6',
+            '360g environ',
+            'chipolatas.jpg',
+            'chipolatas'
         ],
         [
-            'Chipolatas aux herbes de Provence', ' conditionnées sous vide par 6 poids 360g environ', 'chipolatas_au_herbe_de_provences.jpg', 'chipolatas_au_herbe_de_provences'
+            'Chipolatas aux herbes de Provence',
+            'Sous vide par lot de 6',
+            '360g environ',
+            'chipolatas_au_herbe_de_provences.jpg',
+            'chipolatas_au_herbe_de_provences'
         ],
         [
-            'Chipolatas épices merguez', 'conditionné sous vide par 6, poids 360g environ', 'chipolats_epices_merguez.jpg', 'chipolats_epices_merguez'
+            'Chipolatas épices merguez',
+            'Sous vide par lot de  6',
+            '360g environ',
+            'chipolats_epices_merguez.jpg',
+            'chipolats_epices_merguez'
         ],
         [
-            "Chipolatas au piment d'espelette", "conditionnées sous vide par 6 poids 360g environ", 'chipolatas_au_piment_espelette.jpg', 'chipolatas_au_piment_espelette'
+            "Chipolatas au piment d'espelette",
+            "Sous vide par lot de 6",
+            "360g environ",
+            'chipolatas_au_piment_espelette.jpg',
+            'chipolatas_au_piment_espelette'
         ],
         [
-            'Côtes premières', ' conditionnées par 2 sous vide poids 400g environ', 'cote_premiere.jpg', 'cote_premiere'
+            'Côtes premières',
+            'Sous vide par lot de 2',
+            '400g environ',
+            'cote_premiere.jpg',
+            'cote_premiere'
         ],
         [
-            'Crépinettes conditionnées', ' par 2 sous vide poids: 360g', 'crepinettes.jpg', 'crepinettes'
+            'Crépinettes conditionnées',
+            'Sous vide par lot de  2',
+            '360g',
+            'crepinettes.jpg',
+            'crepinettes'
         ],
         [
-            'Escalopes', ' conditionnées sous vide par 4 ou 5 poids 500g environ', 'escalopes.jpg', 'escalopes'
+            'Escalopes',
+            'Sous vide par lot de  4 ou 5',
+            '500g environ',
+            'escalopes.jpg',
+            'escalopes'
         ],
         [
-            'Filet mignon', ' conditionné sous vide poids: 650g environ', 'filet_mignion.jpg', 'filet_mignion'
+            'Filet mignon',
+            'Sous vide',
+            '650g environ',
+            'filet_mignion.jpg',
+            'filet_mignion'
         ],
         [
-            'Foie', ' foie tranché par 3 ou 4 sous vide poids: 350g', 'foie_tranché.jpg', 'foie_tranché'
+            'Foie',
+            'Sous vide par lot de  3 ou 4',
+            '350g',
+            'foie_tranché.jpg',
+            'foie_tranché'
         ],
         [
-            'Jambon blanc 2 tranches', ' conditionné sous vide par 2 tranches poids: 160g', 'jambon_blanc_2_tranches.jpg', 'jambon_blanc_2_tranches'
+            'Jambon blanc 2 tranches',
+            'Sous vide par  lot de 2 tranches',
+            '160g',
+            'jambon_blanc_2_tranches.jpg',
+            'jambon_blanc_2_tranches'
         ],
         [
-            'Jambon blanc 4 tranches', ' conditionné sous vide par 4 tranches: poids: 320g', 'jambon_blanc_4_tranches.jpg', 'jambon_blanc_4_tranches'
+            'Jambon blanc 4 tranches',
+            'Sous vide par lot de 4 tranches',
+            '320g',
+            'jambon_blanc_4_tranches.jpg',
+            'jambon_blanc_4_tranches'
         ],
         [
-            'Jambon blanc 6 tranches', ' conditionné sous vide par 6 tranches: poids: 480g', 'jambon_blanc_6_tranches.jpg', 'jambon_blanc_6_tranches'
+            'Jambon blanc 6 tranches',
+            'Sous vide par lot de 6 tranches',
+            'poids: 480g',
+            'jambon_blanc_6_tranches.jpg',
+            'jambon_blanc_6_tranches'
         ],
         [
-            'Jambon sec', ' conditionné par 6 tranches sous vide poids: 120g', 'jambon_sec_6_tranches.jpg', 'jambon_sec_6_tranches'
+            'Jambon sec',
+            'Sous vide par lot de 6 tranches ',
+            '120g',
+            'jambon_sec_6_tranches.jpg',
+            'jambon_sec_6_tranches'
         ],
         [
-            'Jarret', ' conditionné sous vide poids: 1,200g', 'jarret_de_porc.jpg', 'jarret_de_porc'
+            'Jarret',
+            'Sous vide ',
+            '1,200g',
+            'jarret_de_porc.jpg',
+            'jarret_de_porc'
         ],
         [
-            'Joues', ' conditionnées par 2 sous vide poids: 180g', 'joue.jpg', 'joue'
+            'Joues',
+            'Sous vide par lot de  2',
+            ' 180g',
+            'joue.jpg',
+            'joue'
         ],
         [
-            'Langue', 'conditionnée sous vide', 'langues.jpg', 'langues'
+            'Langue',
+            'Sous vide',
+            '1600g environ',
+            "langues.jpg",
+            'langues'
         ],
         [
-            'Pieds de porc', ' conditionnés sous-vide par 2', 'pieds.jpg', 'pieds'
+            'Pieds de porc',
+            'Sous-vide par lot de 2',
+            '500g environ',
+            'pieds.jpg',
+            'pieds'
         ],
         [
-            'Poitrine', ' conditionnée sous vide poids: 1kg', 'poitrine_morceaux.jpg', 'poitrine_morceaux'
+            'Poitrine',
+            'Sous vide',
+            '1kg',
+            'poitrine_morceaux.jpg',
+            'poitrine_morceaux'
         ],
         [
-            'Poitrine tranchée ou grillades', ' conditionnées sous vide par 5 tranches poids: 500g environ', 'grillade.jpg', 'grillade'
+            'Poitrine tranchée ou grillades',
+            'Sous vide par lot de  5 tranches',
+            '500g environ',
+            'grillade.jpg',
+            'grillade'
         ],
         [
-            'Rognons', ' conditionnés par 2 sous vide', 'rogons_de_porcs.png', 'rogons_de_porcs'
+            'Rognons',
+            'Sous vide par lot de 2',
+            '200g environ',
+            'rogons_de_porcs.png',
+            'rogons_de_porcs'
         ],
         [
-            "Rôti dans le filet ou dans l'échine", "conditionné sous vide poids: 1 kg", 'roti_de_porc.jpg', 'roti_de_porc'
+            "Rôti dans le filet ou dans l'échine",
+            "Sous vide",
+            '1 kg',
+            'roti_de_porc.jpg',
+            'roti_de_porc'
         ],
         [
-            "Rouelle de jambon", " conditionné sous vide poids: 1,400 kg", 'rouelle_de_jambon.jpg', 'rouelle_de_jambon'
+            "Rouelle de jambon",
+            "Sous vide",
+            "1,400 kg",
+            'rouelle_de_jambon.jpg',
+            'rouelle_de_jambon'
         ],
         [
-            "Rouelle d'épaule", " conditionné sous vide poids: 1,200kg", 'rouelle_epaules.jpg', 'rouelle_epaules'
+            "Rouelle d'épaule",
+            "Sous vide",
+            "1,200kg",
+            'rouelle_epaules.jpg',
+            'rouelle_epaules'
         ],
         [
-            "Saucise paysanne Poitrine", " conditionnée sous vide poids: 1kg", 'saucisses_pyasane.jpg', 'saucisses_pyasane'
+            "Saucise paysanne Poitrine",
+             "Sous vide",
+             "1kg",
+             'saucisses_pyasane.jpg',
+             'saucisses_pyasane'
         ],
         [
-            "Saucise de Toulouse  Poitrine", " conditionnée sous vide poids: 1kg", 'saucisses_de_toulouse.jpg', 'saucisses_de_toulouse'
+            "Saucise de Toulouse  Poitrine",
+            "Sous vide",
+            "1kg",
+            'saucisses_de_toulouse.jpg',
+            'saucisses_de_toulouse'
         ],
         [
-            "Sauté de porc en brochettes ou sauté", " conditionnés par 500g sous vide", 'saute_de_porc.jpg', 'saute_de_porc'
+            "Sauté de porc en brochettes ou sauté",
+            "Sous vide",
+            "500g",
+            'saute_de_porc.jpg',
+            'saute_de_porc'
         ],
         [
-            "Travers de porc", "conditionné sous vide poids: 850g environ", 'travers.jpg', 'travers'
+            "Travers de porc",
+            "Sous vide",
+            "850g environ",
+            'travers.jpg',
+            'travers'
         ]
     ]
     ?>
@@ -101,10 +217,11 @@
     <div class="card">
         @foreach($list as $key)
             <div class="card_element">
-                <img src="{{ asset('/img/viande_detail/' .$key[2]) }}" alt="{{ $key[3] }}" class="element_img">
+                <img src="/img/viande_detail/{{ $key[3] }}" alt="{{ $key[4] }}" class="element_img">
                 <div class="card_sub_element">
-                    <h3 class="title_box">{{ $key[0] }}</h3>
-                    <p class="text_box">Les produits sont {{$key[1]}}</p>
+                    <h3>{{ $key[0] }}</h3>
+                    <p><strong>Conditionner :</strong> {{ $key[1] }}</p>
+                    <p><strong>Poids :</strong> {{ $key[2] }}</p>
                 </div>
             </div>
         @endforeach
