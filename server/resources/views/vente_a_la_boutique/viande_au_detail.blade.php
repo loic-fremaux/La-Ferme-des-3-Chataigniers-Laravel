86c06dcad5bfbae199b1f75d2ea20e6d6065334e
Loïc Frémaux <loic.fremaux1@gmail.com>
2020-09-04 16:10:20 +0200

    Fixes

--- server/resources/views/vente_a_la_boutique/viande_au_detail.blade.php
+++ server/resources/views/vente_a_la_boutique/viande_au_detail.blade.php
@@ -11,49 +11,49 @@
         [
             'Boudin noir',
             'Sous vide par lot de 2 ',
-            '360g environ',
+            '360g',
             'boudin_noir.jpg',
             'boudin blanc'
         ],
         [
             'Chair à saucisse',
             'Sous vide',
-            '500g environ',
+            '500g',
             'chaire_saucisse.jpg',
             'chaire a saucisse'
         ],
         [
             'Chipolatas',
             'Sous vide par lot de 6',
-            '360g environ',
+            '360g',
             'chipolatas.jpg',
             'chipolatas'
         ],
         [
             'Chipolatas aux herbes de Provence',
             'Sous vide par lot de 6',
-            '360g environ',
+            '360g',
             'chipolatas_au_herbe_de_provences.jpg',
             'chipolatas_au_herbe_de_provences'
         ],
         [
             'Chipolatas épices merguez',
             'Sous vide par lot de  6',
-            '360g environ',
+            '360g',
             'chipolats_epices_merguez.jpg',
             'chipolats_epices_merguez'
         ],
         [
             "Chipolatas au piment d'espelette",
             "Sous vide par lot de 6",
-            "360g environ",
+            "360g",
             'chipolatas_au_piment_espelette.jpg',
             'chipolatas_au_piment_espelette'
         ],
         [
             'Côtes premières',
             'Sous vide par lot de 2',
-            '400g environ',
+            '400g',
             'cote_premiere.jpg',
             'cote_premiere'
         ],
@@ -67,14 +67,14 @@
         [
             'Escalopes',
             'Sous vide par lot de  4 ou 5',
-            '500g environ',
+            '500g',
             'escalopes.jpg',
             'escalopes'
         ],
         [
             'Filet mignon',
             'Sous vide',
-            '650g environ',
+            '650g',
             'filet_mignion.jpg',
             'filet_mignion'
         ],
@@ -86,25 +86,11 @@
             'foie_tranché'
         ],
         [
-            'Jambon blanc 2 tranches',
+            'Jambon blanc tranché',
             'Sous vide par  lot de 2 tranches',
             '160g',
             'jambon_blanc_2_tranches.jpg',
             'jambon_blanc_2_tranches'
-        ],
-        [
-            'Jambon blanc 4 tranches',
-            'Sous vide par lot de 4 tranches',
-            '320g',
-            'jambon_blanc_4_tranches.jpg',
-            'jambon_blanc_4_tranches'
-        ],
-        [
-            'Jambon blanc 6 tranches',
-            'Sous vide par lot de 6 tranches',
-            'poids: 480g',
-            'jambon_blanc_6_tranches.jpg',
-            'jambon_blanc_6_tranches'
         ],
         [
             'Jambon sec',
@@ -116,63 +102,63 @@
         [
             'Jarret',
             'Sous vide ',
-            '1,200g',
+            '1,200kg',
             'jarret_de_porc.jpg',
             'jarret_de_porc'
         ],
         [
             'Joues',
             'Sous vide par lot de  2',
-            ' 180g',
+            '180g',
             'joue.jpg',
             'joue'
         ],
         [
             'Langue',
             'Sous vide',
-            '1600g environ',
+            '1,600kg',
             "langues.jpg",
             'langues'
         ],
         [
             'Pieds de porc',
             'Sous-vide par lot de 2',
-            '500g environ',
+            '500g',
             'pieds.jpg',
             'pieds'
         ],
         [
             'Poitrine',
             'Sous vide',
-            '1kg',
+            '1,000kg',
             'poitrine_morceaux.jpg',
             'poitrine_morceaux'
         ],
         [
             'Poitrine tranchée ou grillades',
             'Sous vide par lot de  5 tranches',
-            '500g environ',
+            '500g',
             'grillade.jpg',
             'grillade'
         ],
         [
             'Rognons',
             'Sous vide par lot de 2',
-            '200g environ',
+            '200g',
             'rogons_de_porcs.png',
             'rogons_de_porcs'
         ],
         [
             "Rôti dans le filet ou dans l'échine",
             "Sous vide",
-            '1 kg',
+            '1,000kg',
             'roti_de_porc.jpg',
             'roti_de_porc'
         ],
         [
             "Rouelle de jambon",
             "Sous vide",
-            "1,400 kg",
+            "1,400kg",
             'rouelle_de_jambon.jpg',
             'rouelle_de_jambon'
         ],
@@ -207,7 +193,7 @@
         [
             "Travers de porc",
             "Sous vide",
-            "850g environ",
+            "850g",
             'travers.jpg',
             'travers'
         ]
@@ -220,7 +206,7 @@
                 <img src="/img/viande_detail/{{ $key[3] }}" alt="{{ $key[4] }}" class="small_element_img">
                 <div class="card_sub_element">
                     <h3>{{ $key[0] }}</h3>
-                    <p><strong>Poids :</strong> {{ $key[2] }}</p>
+                    <p><strong>Poids :</strong> {{ $key[2] }} environ</p>
                 </div>
             </div>
         @endforeach
