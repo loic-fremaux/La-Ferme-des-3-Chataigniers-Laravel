dd145b15e63274381989526f558ce77aec2708d5
Loïc Frémaux <loic.fremaux1@gmail.com>
2020-09-04 16:48:18 +0200

    Implement auth / admin

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,20 +18,28 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::get('/elevage', function () {
+Route::get('elevage', function () {
     return view('elevage');
 })->name('elevage');
 
 Route::name('vente_a_la_boutique.')->prefix('vente_a_la_boutique')->group(function () {
     Route::get('/', function () {
-        return view('vente_a_la_boutique/index');
+        return view('vente_a_la_boutique.index');
     })->name('index');
 
-    Route::get('/pates_et_plats_cuisines', function () {
-        return view('/vente_a_la_boutique/pates_et_plats_cuisines');
+    Route::get('pates_et_plats_cuisines', function () {
+        return view('vente_a_la_boutique.pates_et_plats_cuisines');
     })->name('pates_et_plats_cuisines');
 
-    Route::get('/viande_au_detail', function () {
-        return view('/vente_a_la_boutique/viande_au_detail');
+    Route::get('viande_au_detail', function () {
+        return view('vente_a_la_boutique.viande_au_detail');
     })->name('viande_au_detail');
 });
+
+Route::name('admin.')->prefix('admin')->middleware(['auth'])->group(function() {
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+});
+
+Auth::routes();
